34955d1ce81349c7187af42a330588e76cbfacdb
ignacio Carvajal <ignacio.carvajal@alumnos.ucn.cl>
2025-12-18 17:21:23 -0300

    feat: added new frontend url to the CORS policy

--- Cubitwelve/Program.cs
+++ Cubitwelve/Program.cs
@@ -26,7 +26,8 @@ builder.Services.AddCors(options =>
                                 .WithOrigins("https://cubi12.azurewebsites.net",
                                             "https://cubi12.cl",
                                             "https://www.cubi12.cl",
-                                            "https://cubi12-front-deploy-latest.onrender.com"
+                                            "https://cubi12-front-deploy-latest.onrender.com",
+                                            "https://cubi12-front-1dw4.onrender.com"
                                             );
                       });
 });
