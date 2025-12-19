e83c314216ba649a3e80f75d42b5b9ab3c6b914c
ignacio Carvajal <ignacio.carvajal@alumnos.ucn.cl>
2025-12-18 22:02:39 -0300

    test

--- Cubitwelve/Program.cs
+++ Cubitwelve/Program.cs
@@ -32,7 +32,7 @@ builder.Services.AddCors(options =>
                       });
 });
 
-// Add services to the container.
+// Add services to t2he container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
